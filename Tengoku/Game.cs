446c7ed3b3deaf6f9c647e1095499f1bfb190f1c
Braedon Lewis <braedonwilcox@gmail.com>
2023-02-15 20:52:59 -0500

    Add "ImGui"

--- Tengoku/Game.cs
+++ Tengoku/Game.cs
@@ -3,6 +3,8 @@
 using Raylib_CsLo;
 using Tengoku.Games;
 
+using ImGuiNET;
+
 namespace Tengoku
 {
     public class Game : TrinkitApp
@@ -17,6 +19,8 @@ namespace Tengoku
         {
             Raylib.InitAudioDevice();
 
+            TrinkitImGui.Setup(false);
+
             spaceball = new Spaceball();
         }
 
@@ -30,12 +34,54 @@ namespace Tengoku
             Raylib.ClearBackground(Raylib.WHITE);
 
             spaceball.Draw();
+
+            TrinkitImGui.Begin();
+            
+            if (ImGui.BeginMainMenuBar())
+            {
+                if (ImGui.BeginMenu("Window"))
+                {
+                    if (ImGui.MenuItem("Resolution")) { }
+                    ImGui.EndMenu();
+                }
+                if (ImGui.BeginMenu("Audio"))
+                {
+                    if (ImGui.MenuItem("Latency")) { }
+                    ImGui.EndMenu();
+                }
+                if (ImGui.BeginMenu("Scenes")) 
+                {
+                    if (ImGui.MenuItem("Splashscreen")) { }
+                    if (ImGui.MenuItem("Title")) { }
+                    if (ImGui.MenuItem("Menu")) { }
+                    if (ImGui.MenuItem("GameSelect")) { }
+                    if (ImGui.MenuItem("Game")) { }
+                    ImGui.EndMenu();
+                }
+                if (ImGui.BeginMenu("Games"))
+                {
+                    if (ImGui.MenuItem("Load Custom...")) { }
+                    ImGui.Separator();
+                    if (ImGui.MenuItem("Karate Man")) { }
+                    if (ImGui.MenuItem("Rhythm Tweezers")) { }
+                    if (ImGui.MenuItem("Marching Orders")) { }
+                    if (ImGui.MenuItem("Spaceball")) { }
+                    if (ImGui.MenuItem("The Clappy Trio")) { }
+                    if (ImGui.MenuItem("Remix 1")) { }
+                    ImGui.EndMenu();
+                }
+
+                ImGui.EndMainMenuBar();
+            }
+
+            TrinkitImGui.End();
         }
 
         public override void OnQuit()
         {
             spaceball.Dispose();
 
+            TrinkitImGui.Shutdown();
             Raylib.CloseAudioDevice();
         }
     }
